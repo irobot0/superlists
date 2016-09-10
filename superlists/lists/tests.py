e2a0e8dc28fd6ee2a3787f4c6443531ea4f9e2d8
xuyan <informlarry@163.com>
2016-09-10 14:00:33 +0800

    5.2  Processing a POST Request on the Server

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -17,3 +17,11 @@ class HomePageTest(TestCase):
         response = home_page(request)
         expected_html = render_to_string('lists/home.html')
         self.assertEqual(expected_html, response.content.decode())
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())
