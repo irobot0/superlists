fcade094f6d3e1dd8d6cecb44fe28a46405340c9
xuyan <informlarry@163.com>
2016-09-10 15:45:55 +0800

    5.5  The Django ORM and Our First Model

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,6 +3,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 
+from .models import Item
 from .views import home_page
 
 
@@ -32,3 +33,23 @@ class HomePageTest(TestCase):
             }
         )
         self.assertEqual(expected_html, response.content.decode())
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual('The first (ever) list item', first_saved_item.text)
+        self.assertEqual('Item the second', second_saved_item.text)
